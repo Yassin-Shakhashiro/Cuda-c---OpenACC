c7677e2c4a9d5028edc83f6e10c72bfa8d240c8b
Yassin Shakhashiro <157815039+Yassin-Shakhashiro@users.noreply.github.com>
2024-04-24 00:40:28 +0300

    Update Tiled_Matrix_Multiplication_Cuda C.c

--- Tiled_Matrix_Multiplication_Cuda C.c
+++ Tiled_Matrix_Multiplication_Cuda C.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TILE_mat_width 16  // Assuming block size is 16x16
+#define TILE_mat_width 16  
 
 __global__ void matrixMulTiled(float *A, float *B, float *C, int mat_width) {
     __shared__ float tileA[TILE_mat_width][TILE_mat_width];
@@ -34,38 +34,36 @@ int main() {
     float *h_A, *h_B, *h_C;  // Host copies of A, B, C
     float *d_A, *d_B, *d_C;  // Device copies of A, B, C
 
-    // Allocate space for host copies of A, B, C and setup input values
     h_A = (float *)malloc(size);
     h_B = (float *)malloc(size);
     h_C = (float *)malloc(size);
 
-    // Initialize matrices
     for (int i = 0; i < mat_width * mat_width; i++) {
         h_A[i] = rand() / (float)RAND_MAX;
         h_B[i] = rand() / (float)RAND_MAX;
     }
 
-    // Allocate space for device copies of A, B, C
     cudaMalloc((void **)&d_A, size);
     cudaMalloc((void **)&d_B, size);
     cudaMalloc((void **)&d_C, size);
 
-    // Copy inputs to device
     cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
-    // Launch matrixMulTiled() kernel on the GPU
     dim3 dimBlock(TILE_mat_width, TILE_mat_width);
     dim3 dimGrid((mat_width + TILE_mat_width - 1) / TILE_mat_width, (mat_width + TILE_mat_width - 1) / TILE_mat_width);
-
+    clock_t t;
+    t = clock();
     matrixMulTiled<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, mat_width);
-
-    // Copy result back to host
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; 
+    printf("%lf",time_taken);
     cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-    // Cleanup
     cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
     free(h_A); free(h_B); free(h_C);
 
     return 0;
 }
+
+
